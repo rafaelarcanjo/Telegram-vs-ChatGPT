1b3fd1e5bab92de28a2cb5f6310652efebf5d78c
Rafael Silva <rafael.silva@alfasoft.pt>
2023-06-20 20:58:58 -0300

    [FEAT] Adicionando mensagens de exceções

--- chat.py
+++ chat.py
@@ -12,17 +12,22 @@ class ChatGPT:
 
         model_engine = "text-davinci-003"
 
-        completions = openai.Completion.create(
-            engine=model_engine,
-            prompt=chat,
-            max_tokens=512,
-            temperature=0.1,
-            top_p=1.0,
-            frequency_penalty=0.5,
-            presence_penalty=1,
-            n=1,
-            stop=None
-        )
+        try:
+            completions = openai.Completion.create(
+                engine=model_engine,
+                prompt=chat,
+                max_tokens=512,
+                temperature=0.1,
+                top_p=1.0,
+                frequency_penalty=0.5,
+                presence_penalty=1,
+                n=1,
+                stop=None
+            )
 
-        return completions.choices[0].text[:4000]
+            return completions.choices[0].text[:4000]
+        except openai.error.AuthenticationError as e:
+            return "Erro na autenticação com o OpenAI :/"
+        except Exception as e:
+            return "Algo de errado não está certo :/"
     # END
